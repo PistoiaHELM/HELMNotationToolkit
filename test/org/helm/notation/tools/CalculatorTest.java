04f490620d628a67b9c598aa41b32f179c50cd6e
maisel <maisel@qr042.quattro.local>
2014-02-10 11:41:42 +0100

    SM: Calculator Unit Test

--- test/org/helm/notation/tools/CalculatorTest.java
+++ test/org/helm/notation/tools/CalculatorTest.java
@@ -46,12 +46,69 @@ public class CalculatorTest {
         assertEquals(result,5500.0,1e-15);
 	}
 	
-	
+	@Test
 	public void testCalculateFromPeptidePolymerNotation() throws NotationException, MonomerException, CalculationException, IOException, JDOMException, StructureException{
 		String input = "A.G.G.W.E.E.E.E.E.W";
         float result = calculator.calculateFromPeptidePolymerNotation(input);
         assertEquals(result,11000.0,1e-15);
         
+		
+	}
+	
+	@Test
+	public void testCalculateFromComplexNotation() throws NotationException, MonomerException, IOException, JDOMException, StructureException, CalculationException{
+		String input = "PEPTIDE1{A.G.G.W.E.E.E.E.E.W}$$$$";
+        float result = calculator.calculateFromComplexNotation(input, ExtinctionCoefficientCalculator.PEPTIDE_UNIT_TYPE);
+        assertEquals(result,11000.0,1e-15);
+    	
+        input = "PEPTIDE1{A.G.G.W.E.E.E.E.E.W}|PEPTIDE2{A.G.G.W.E.Y.E.E.E.E.W}$$$$";
+        result = calculator.calculateFromComplexNotation(input);        
+        assertEquals(result,23.49,1e-6);
+        
+        
+        input = "PEPTIDE1{A.G.G.W.E.E.E.E.E.W}|PEPTIDE2{A.G.G.W.E.Y.E.E.E.E.W}$$$$";
+        result = calculator.calculateFromComplexNotation(input, ExtinctionCoefficientCalculator.PEPTIDE_UNIT_TYPE);        
+        assertEquals(result,23490.0,1e-15);
+        
+        
+        input = "RNA1{P.R(A)P.R([5meC])P.R(G)P.[mR](A)}$$$$";
+        result = calculator.calculateFromComplexNotation(input);
+        assertEquals(result,46.200005,1e-6);
+       
+        input = "RNA1{P.R(A)P.R([5meC])P.R(G)P.[mR](A)}$$$$";
+        result = calculator.calculateFromComplexNotation(input, ExtinctionCoefficientCalculator.PEPTIDE_UNIT_TYPE);
+        assertEquals(result,46200.004,1e-4);
+        
+        input = "RNA1{P.R(A)P.R([5meC])P.R(G)P.[mR](A)}|CHEM1{PEG3}|PEPTIDE1{A.G.G.W.E.E.E.E.E.W}|PEPTIDE2{A.G.G.W.E.Y.E.E.E.E.W}$$$$";
+        result = calculator.calculateFromComplexNotation(input);
+        assertEquals(result,69.69,1e-5);
+        
+        
+        
+	}
+	
+	@Test
+	public void testCalculateFromNucleotideSequence() throws CalculationException{
+		String input = "ACGTACGT";
+        float result = calculator.calculateFromNucleotideSequence(input);
+        assertEquals(result, 81.119995,1e-6);
+        
+	}
+	
+	@Test
+	public void testCalculateFromModifiedNucleotideSequence() throws CalculationException, NotationException, IOException, JDOMException{
+        String input = "ACGmTACmGT";
+        float result = calculator.calculateFromModifiedNucleotideSequence(input);
+        assertEquals(result, 81.119995,1e-6);       
+
+		
+	}
+	
+	@Test
+	public void testCalculateFromRnaPolymerNotation() throws NotationException, MonomerException, CalculationException, IOException, JDOMException, StructureException{
+	    String input = "P.R(A)P.R(C)P.R(G)P.[mR](A)";
+        float result = calculator.calculateFromRnaPolymerNotation(input);
+        assertEquals(result, 46.200005,1e-6);        
 		
 	}
 	
