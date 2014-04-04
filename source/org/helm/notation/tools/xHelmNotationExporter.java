3594b06cb1a514cd074e5afacfcaf99646c91cd3
maisel <maisel@qr042.quattro.local>
2014-04-04 14:37:14 +0200

    -Moved writeHelm function to exporter class -Fix for bug xhelm-30 and xhelm-31

--- source/org/helm/notation/tools/xHelmNotationExporter.java
+++ source/org/helm/notation/tools/xHelmNotationExporter.java
@@ -4,13 +4,16 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.helm.notation.MonomerException;
 import org.helm.notation.MonomerStore;
 import org.helm.notation.model.Monomer;
+import org.helm.notation.model.PolymerNode;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.output.Format;
@@ -24,53 +27,58 @@ public class xHelmNotationExporter {
   
     public static final String HELM_NOTATION_ELEMENT 	= "HelmNotation";
     
-     
+
 	
-	 public static Document buildXHelmDocument(String helmNotation,MonomerStore monomerStore) throws MonomerException {
-	        Element root = new Element(XHELM_ELEMENT);
-			
-	        Document doc = new Document(root);
+	public static String writeXHELM(String helmNotation, MonomerStore store) {
+		Element root = new Element(XHELM_ELEMENT);
 
-			
-			
-	        Element helmElement=new Element(HELM_NOTATION_ELEMENT);
-	        helmElement.setText(helmNotation);	        
-	        
-	        root.addContent(helmElement);
-	       
-	        Element monomerListElement = new Element(MONOMER_LIST_ELEMENT);
-	        Set<String> polymerTypeSet = monomerStore.getPolymerTypeSet();
-	        for (Iterator<String> i = polymerTypeSet.iterator();  i.hasNext();) {
-	            String polymerType = i.next();
-	  
-	            Map<String, Monomer> monomerMap = monomerStore.getMonomers(polymerType);
-	                 
-	            Collection<Monomer> monomers=monomerMap.values();
-	            for (Iterator<Monomer> iterator = monomers.iterator(); iterator
-						.hasNext();) {
-					Monomer monomer = iterator.next();
-					Element monomerElement = MonomerParser.getMonomerElement(monomer);
-					monomerListElement.getChildren().add(monomerElement);
-				}	              
-	        }
-	     
-	        root.addContent(monomerListElement);
+		Document doc = new Document(root);
 
-	        return doc;
-	    }
+		Element helmElement = new Element(HELM_NOTATION_ELEMENT);
+		helmElement.setText(helmNotation);
+
+		root.addContent(helmElement);
+
+		Element monomerListElement = new Element(MONOMER_LIST_ELEMENT);
+
+		try {
+			// get all simple polymers
+			Set<Monomer> set = new HashSet<Monomer>();
+			List<PolymerNode> simplePolymers = ComplexNotationParser
+					.getPolymerNodeList(helmNotation);
+			for (PolymerNode node : simplePolymers) {
+				List<Monomer> monomers = SimpleNotationParser.getMonomerList(
+						node.getLabel(), node.getType(), store);
+				for (Monomer subnode : monomers) {
+					if (!subnode.getAlternateId().startsWith("AM#")) {
+						set.add(subnode);
+					}
+				}
+
+			}
+			// Distinct monomers
+			for (Monomer distinctmonomer : set) {
+				Element monomerElement = MonomerParser
+						.getMonomerElement(distinctmonomer);
+				monomerListElement.getChildren().add(monomerElement);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		root.addContent(monomerListElement);
+
+		XMLOutputter xmlOutput = new XMLOutputter();
+		// display nice
+		xmlOutput.setFormat(Format.getPrettyFormat());
+
+		return xmlOutput.outputString(doc);
+
+	}
 	 
-	 	public static void saveXHelmDocument( Document document, File file) throws IOException {
-	 		XMLOutputter xmlOutput = new XMLOutputter();	 
-			// display nice 
-			xmlOutput.setFormat(Format.getPrettyFormat());
-		
-	 		FileOutputStream fos = new FileOutputStream(file);
-	 		xmlOutput.output(document, fos);
-	 	}
-	 
-	 	public static void saveXHelm(String helmNotation,MonomerStore monomerStore,File file)  throws IOException, MonomerException {
-	 		Document xHelm = buildXHelmDocument(helmNotation,monomerStore);
-	        saveXHelmDocument( xHelm, file);  
-	    }
+	 	
 	
 }
--- source/org/helm/notation/tools/xHelmNotationParser.java
+++ source/org/helm/notation/tools/xHelmNotationParser.java
@@ -41,46 +41,6 @@ public class xHelmNotationParser {
 		return monomerStore;
 	}
 
-	public static String writeXHELM(String xHelmCode, MonomerStore store) {
-		String xml = "";
-		try {
 
-			xml += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-					+ System.getProperty("line.separator");;
-			xml += "<Xhelm>" + System.getProperty("line.separator");;
-			xml += "	<HelmNotation>" + xHelmCode + "</HelmNotation>"
-					+ System.getProperty("line.separator");
-			xml += "<Monomers>" + System.getProperty("line.separator");
-
-			// get all simple polymers
-			Set<Monomer> set = new HashSet<Monomer>();
-			List<PolymerNode> simplePolymers = ComplexNotationParser
-					.getPolymerNodeList(xHelmCode);
-			for (PolymerNode node : simplePolymers) {
-				List<Monomer> monomers = SimpleNotationParser.getMonomerList(
-						node.getLabel(), node.getType(), store);
-				for (Monomer subnode : monomers) {
-					set.add(subnode);
-				}
-
-			}
-			// Distinct monomers
-			for (Monomer distinctmonomer : set) {
-				XMLOutputter outp = new XMLOutputter();
-				String s = outp.outputString(MonomerParser
-						.getMonomerElement(distinctmonomer));
-				xml += "	" + s + System.getProperty("line.separator")
-						+ System.getProperty("line.separator");
-
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-
-		xml += "</Monomers>" + System.getProperty("line.separator");
-		xml += "</Xhelm>" + System.getProperty("line.separator");
-		return xml;
-	}
 
 }
